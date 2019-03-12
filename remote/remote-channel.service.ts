fe6ce27a6e197c67ed408510c1f5acd84321b593
gorebill <gorebill@163.com>
2019-03-12 21:08:08 +0800

    change to Observable in channel service

--- remote/remote-channel.service.ts
+++ remote/remote-channel.service.ts
@@ -1,17 +1,21 @@
-import {Injectable} from '@angular/core';
-import {HttpClient, HttpParams, HttpHeaders} from '@angular/common/http';
-import {UbibotCommonConfigService} from '../providers/ubibot-common-config.service';
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+import { UbibotCommonConfigService } from '../providers/ubibot-common-config.service';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class RemoteChannelService {
 
     constructor(private http: HttpClient,
-                private ubibotCommonConfig: UbibotCommonConfigService) {
+        private ubibotCommonConfig: UbibotCommonConfigService) {
     }
 
-    list(): Promise<any> {
+    list(): Observable<any> {
         let url = `${this.ubibotCommonConfig.EndPoint}/channels`;
-        return this.http.get(url).toPromise();
+        return this.http.get(url).pipe(
+            map((resp: any) => resp.channels)
+        );//.subscribe(resp => resp)
     }
 
 }
