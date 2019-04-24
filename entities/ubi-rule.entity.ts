d14c04098d363dd181c28b324b33fefc6141e6de
gorebill <gorebill@163.com>
2019-04-24 18:31:07 +0800

    update

---
+++ entities/ubi-rule.entity.ts
@@ -0,0 +1,128 @@
+import { UbiChannel } from "./ubi-channel.entity";
+
+export interface UbiRuleExtra {
+    channel: UbiChannel;
+}
+
+export enum UbiRuleType {
+    NO_DATA_CHECK = 'no_data_check',
+    NUMERIC = 'numeric',
+    STATUS_FIELD = 'status_field',
+}
+
+export enum UbiRuleStatus {
+    ENABLED = 'enabled',
+    DISABELD = 'disabled',
+}
+
+export class UbiRule {
+    action_frequency?: any;
+    action_sub_value?: any;
+    action_sub_value2?: any;
+    action_sub_value3?: any;
+    action_sub_value4?: any;
+    action_sub_value5?: any;
+    action_type?: any;
+    action_type2?: any;
+    action_type3?: any;
+    action_type4?: any;
+    action_type5?: any;
+    action_value?: any;
+    action_value2?: any;
+    action_value3?: any;
+    action_value4?: any;
+    action_value5?: any;
+    batch_id?: any;
+    channel_id?: any;
+    created_at?: any;
+    criteria?: any;
+    field?: any;
+    field_name?: any;
+    frequency?: any;
+    frequency_value?: any;
+    last_result?: any;
+    last_result_time?: any;
+    latitude?: any;
+    longitude?: any;
+    r_end?: any;
+    r_start?: any;
+    recover_action?: any;
+    rule_condition?: any;
+    rule_id?: any;
+    rule_name?: any;
+    rule_status?: UbiRuleStatus;
+    rule_type?: UbiRuleType;
+    updated_at?: any;
+    user_id?: any;
+
+
+    // for extra info
+    // [key: string]: any;
+
+    private extra: UbiRuleExtra;
+
+
+    constructor(raw: any = {}, channel) {
+        Object.setPrototypeOf(this, UbiRule.prototype);
+        Object.assign(this, raw);
+
+        // attach channel to rule
+        this.extra = {
+            channel: channel
+        }
+    }
+
+    merge(data: any) {
+        Object.assign(this, data);
+    }
+
+
+    /**
+     * 是否默认rule，默认rule只能修改/toggle，不能删除
+     *
+     * @returns
+     * @memberof UbiRule
+     */
+    isDefaultRule() {
+        return this.frequency === 'device_dp';
+    }
+
+    getChannel() {
+        return this.extra.channel;
+    }
+
+
+    /**
+     * 是否当前版本已定义的rule类型
+     *
+     * @returns
+     * @memberof UbiRule
+     */
+    isRecognizedType() {
+        switch (this.rule_type) {
+            case UbiRuleType.NO_DATA_CHECK:
+            case UbiRuleType.NUMERIC:
+            case UbiRuleType.STATUS_FIELD:
+                return true;
+            default:
+                return false;
+        }
+    }
+
+
+    /**
+     * 是否已激活恢复时报警
+     *
+     * @returns
+     * @memberof UbiRule
+     */
+    isRecoverActionEnabled() {
+        return this.recover_action === 'enabled';
+    }
+
+    isEnabled() {
+        return this.rule_status === UbiRuleStatus.ENABLED;
+    }
+}
+
+
--- remote/remote-channel.service.ts
+++ remote/remote-channel.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { UbibotCommonConfigService } from '../providers/ubibot-common-config.service';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Observable, of, from, combineLatest } from 'rxjs';
+import { map, switchMap, mergeAll, combineAll, concatMap, tap, zipAll, withLatestFrom, take, takeLast } from 'rxjs/operators';
 import { UbiChannelDAO, UbiChannel } from '../entities/ubi-channel.entity';
 import { UbiError } from '../errors/UbiError';
+import { UbiRule } from '../entities/ubi-rule.entity';
 
 @Injectable()
 export class RemoteChannelService {
@@ -36,8 +37,7 @@ export class RemoteChannelService {
      * @memberof RemoteChannelService
      */
     get(channelId: string): Observable<UbiChannel> {
-
-        if(!channelId) throw new UbiError('Channel ID is required for this API!');
+        if (!channelId) throw new UbiError('Channel ID is required for this API!');
 
         let url = `${this.ubibotCommonConfig.EndPoint}/channels/${channelId}`;
         return this.http.get(url).pipe(
@@ -47,4 +47,68 @@ export class RemoteChannelService {
         );
     }
 
+
+    /**
+     * 列出该channel的所有rules并attach相应的channel到extra信息
+     *
+     * List Channel Rules
+     *
+     * To add a channel rule, send an HTTP GET to http://api.datadudu.com/channels/CHANNEL_ID/rules
+     * replacing CHANNEL_ID with the ID of your channel
+     *
+     * Valid request parameters:
+     * api_key or token_id (string) – api_key is Read or Write key for this specific channel (no key required for public channels) or token_id for internal use, obtained through login API. (required)
+     *
+     * @returns {Observable<UbiRule[]>}
+     * @memberof RemoteChannelService
+     */
+    listRules(channelId: string): Observable<UbiRule[]> {
+        if (!channelId) throw new UbiError('Channel ID is required for this API!');
+
+        let url = `${this.ubibotCommonConfig.EndPoint}/channels/${channelId}/rules`;
+        return combineLatest(
+            this.http.get(url),
+            this.get(channelId),
+        ).pipe(
+            switchMap(([resp, channel]: any[]) => {
+                // console.log(resp);
+                return from(resp.rules).pipe(
+                    map<UbiRule, any>(raw => {
+                        return of(new UbiRule(raw, channel));
+                    }),
+                );
+            }),
+            // 根据文档, zipAll的source必须是一个observable of observables / ..., 所以map的时候必须通过of返回
+            // ref: https://github.com/ReactiveX/rxjs/issues/1677
+            zipAll<UbiRule>(),
+        );
+    }
+
+
+    /**
+     *
+     * Update a Rule Status (enable or disable rule)
+     *
+     * To add a channel rule, send an HTTP PUT to http://api.datadudu.cn/channels/CHANNEL_ID/rules/RULE_ID/rule_status
+     * replacing CHANNEL_ID with the ID of your channel, and RULE_ID with ID of the rule.
+     *
+     * Valid request parameters:
+     * account_key or token_id (string) –account_key  or token_id for internal use, obtained through login API. (required)
+     * JSON Body FIELDS:
+     *
+     * rule_status(string): “enabled” or “disabled”
+     *
+     * @param {*} channelId
+     * @param {*} ruleId
+     * @returns {Observable<UbiRule>}
+     * @memberof RemoteChannelService
+     */
+    toggleRuleStatus(channelId, ruleId, status): Observable<any> {
+        if (!ruleId) throw new UbiError('Rule ID is required for this API!');
+
+        let url = `${this.ubibotCommonConfig.EndPoint}/channels/${channelId}/rules/${ruleId}/rule_status`;
+        return this.http.put(url, { 'rule_status': status }).pipe(
+            map((x: any) => x.rule)
+        );
+    }
 }
