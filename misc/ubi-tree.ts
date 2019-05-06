652a6533bdff3fc96b8bc900cce6167fb67eca12
gorebill <gorebill@163.com>
2019-05-07 03:36:58 +0800

    update

--- misc/ubi-tree.ts
+++ misc/ubi-tree.ts
@@ -1,58 +1,127 @@
 import { UbiError } from "../errors/UbiError";
 import { EnumAppError } from "../enums/enum-app-error.enum";
 
-const MAX_DEPTH = 100;
+const MAX_DEPTH = 20;
 
-export interface UbiTree<T> {
-    label: string;
-    value: T;
-    children: UbiTree<T>[];
-}
 
-export class UbiTreeNode<T> implements UbiTree<T> {
-    label: string;
-    value: T;
-    children: UbiTree<T>[] = [];
+/**
+ * Ubi named tree.
+ * 树结构，目前用于flow控制
+ *
+ * @export
+ * @class UbiTree
+ * @template T
+ * @author gorebill
+ */
+export class UbiTree<T> {
+    name: string;
 
-    constructor(label: string, value: T) {
-        this.label = label;
-        this.value = value;
+    private root: UbiTreeNodeImpl<T>;
+
+    constructor(name: string, treeModel: UbiTreeNode<T>) {
+        this.name = name;
+
+        // tag: 根据model构建树
+        const rootNode = this.makeNode(treeModel);
+        this.root = rootNode;
     }
 
-    /**
-     * Include self.
-     *
-     * @param {string} label
-     * @returns {UbiTree<T>}
-     * @memberof UbiTree
-     */
-    findFirstByLabel(label: string): UbiTree<T> {
-        return this.findByLabel(label, this, 0);
-    }
-
-
-    /**
-     * 根据label深度优先
-     *
-     * @private
-     * @param {string} label
-     * @param {UbiTree<T>} tree
-     * @returns {UbiTree<T>}
-     * @memberof UbiTree
-     */
-    private findByLabel(label: string, tree: UbiTree<T>, depth): UbiTree<T> {
-        if (label === tree.label) return tree;
+    private makeNode(treeModel: UbiTreeNode<T>, depth: number = 0): UbiTreeNodeImpl<T> {
 
         if (depth > MAX_DEPTH) throw new UbiError(EnumAppError.EXCEED_MAX_STACK_DEPTH);
 
-        for (let i = 0; i < tree.children.length; i++) {
-            const child = tree.children[i];
-            const found = this.findByLabel(label, child, depth + 1);
+        const node = new UbiTreeNodeImpl(
+            treeModel.key,
+            treeModel.value,
+            null,
+        );
+
+        treeModel.children.forEach((v: UbiTreeNode<T>, i: number) => {
+            const child = this.makeNode(v, depth + 1);
+            child.parent = node;
+            node.children.push(child);
+        });
+
+        return node;
+    }
+
+    /**
+     * Find by key, self inclusive.
+     *
+     * @param {string} key
+     * @returns {UbiTreeNode<T>}
+     * @memberof UbiTree
+     */
+    findByKey(key: string): UbiTreeNode<T> {
+        return this.findNodeByKey(key, this.root);
+    }
+
+    getRoot(): UbiTreeNode<T> {
+        return this.root;
+    }
+
+    /**
+     * If node not found, return null.
+     *
+     * @param {string} key
+     * @returns {UbiTreeNode<T>[]}
+     * @memberof UbiTree
+     */
+    getChildrenOfKey(key: string): UbiTreeNode<T>[] {
+        const node = this.findByKey(key);
+        return node ? node.children : null;
+    }
+
+
+    /**
+     * 根据key深度优先
+     *
+     * @private
+     * @param {string} key
+     * @param {UbiTreeNode<T>} tree
+     * @returns {UbiTreeNode<T>}
+     * @memberof UbiTree
+     */
+    private findNodeByKey(key: string, node: UbiTreeNode<T>, depth: number = 0): UbiTreeNode<T> {
+        console.log(`Matching ${key} with ${node.key}: ${key === node.key}...`);
+
+        if (key === node.key) return node;
+
+        if (depth > MAX_DEPTH) throw new UbiError(EnumAppError.EXCEED_MAX_STACK_DEPTH);
+
+        for (let i = 0; i < node.children.length; i++) {
+            const child = node.children[i];
+            const found = this.findNodeByKey(key, child, depth + 1);
             if (found) {
                 return found;
             }
         }
 
         return null;
+    }
+}
+
+export interface UbiTreeNode<T> {
+    // 由构建器填写
+    parent?: UbiTreeNode<T>;
+
+    key: string;
+    value: T;
+    children: UbiTreeNode<T>[];
+}
+
+class UbiTreeNodeImpl<T> implements UbiTreeNode<T> {
+    parent: UbiTreeNodeImpl<T>;
+
+    key: string;
+    value: T;
+    children: UbiTreeNodeImpl<T>[];
+
+    constructor(key: string, value: T, parent: UbiTreeNodeImpl<T>) {
+        this.key = key;
+        this.value = value;
+        this.children = [];
+
+        this.parent = parent;
     }
 }
