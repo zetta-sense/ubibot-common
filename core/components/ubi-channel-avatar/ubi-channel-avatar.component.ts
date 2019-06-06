6b0173b680e67af6c01ce82b968ac90af1e1400c
gorebill <gorebill@163.com>
2019-06-06 21:20:42 +0800

    update

--- core/components/ubi-channel-avatar/ubi-channel-avatar.component.ts
+++ core/components/ubi-channel-avatar/ubi-channel-avatar.component.ts
@@ -28,6 +28,9 @@ export class UbiChannelAvatarComponent implements OnInit, OnChanges {
     @Input()
     hideStatus: boolean;
 
+    @Input()
+    statusPosition: 'right' | 'left' = 'right';
+
     avatarImage: string;
 
     constructor(private ubiUtils: UbiUtilsService,
