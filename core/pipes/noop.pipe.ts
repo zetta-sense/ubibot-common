ef6c0a0d6261213cde5fc26ad7693c570c11d7cc
gorebill <gorebill@163.com>
2019-09-23 11:32:30 +0800

    update

--- core/pipes/noop.pipe.ts
+++ core/pipes/noop.pipe.ts
@@ -2,6 +2,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 
 /**
+ * @deprecated
+ *
  * Noop Pipe, 仅用于触发额外参数值变动
  * 应只放在pipe链的最后
  *
@@ -14,8 +16,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class NoopPipe implements PipeTransform {
 
-    transform(...args: any[]): any {
-        return args;
+    transform(input: any, ...args: any[]): any {
+        // args仅用于触发变动，不输出
+        return input;
     }
 
 }
