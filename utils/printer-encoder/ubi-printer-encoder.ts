6caf4525a233bd26d097a247d975fb5ad503c252
gorebill <gorebill@163.com>
2020-08-07 00:08:15 +0800

    update

---
+++ utils/printer-encoder/ubi-printer-encoder.ts
@@ -0,0 +1,12 @@
+import { UbiFeedsItem } from "../../remote/remote-channel.service";
+
+export class UbiPrinterEncoder {
+
+    constructor() {
+
+    }
+
+    initWithFeeds(feeds: UbiFeedsItem[]): void {
+        console.log(feeds);
+    }
+}
