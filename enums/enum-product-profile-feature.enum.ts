5c7713590b6e22a01d81856caf0ca6ca22e35630
gorebill <gorebill@163.com>
2021-12-24 13:12:44 +0800

    update comment

--- enums/enum-product-profile-feature.enum.ts
+++ enums/enum-product-profile-feature.enum.ts
@@ -12,6 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+/**
+ * 一旦有新加，要注意channel-settings.component.html添加相应项
+ *
+ * @export
+ * @enum {number}
+ */
 export enum EnumProductProfileFeature {
     ExMqtt = 'ex:mqtt',
 
